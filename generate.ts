c23bbfbabba37bf96abe6067ee59a34a16dc91cd
lsankar4033 <lsankar4033@gmail.com>
2022-06-28 10:35:52 -0400

    filenames in setup scripts

--- generate.ts
+++ generate.ts
@@ -2,11 +2,17 @@ import { readFileSync, writeFileSync } from "fs";
 
 import { buildTreePoseidon } from "./src/merklePoseidon";
 
-const groupId = "daohack";
+const groupId = "dfwinners";
 const buffer = readFileSync(`input/${groupId}_setup.json`);
 const data = JSON.parse(buffer.toString());
 
-buildTreePoseidon(data.addresses, 7, 30, 0n).then((res) => {
+// data.addresses can be a filename or a list of strings
+let addresses = data.addresses;
+if (typeof addresses === "string") {
+  addresses = readFileSync(addresses, "utf8").split(/\r?\n/);
+}
+
+buildTreePoseidon(addresses, 7, 30, 0n).then((res) => {
   console.log(`Constructed tree with root ${res.root}`);
 
   res["groupId"] = data.groupId;
