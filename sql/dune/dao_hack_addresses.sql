f756eae0c2a83021212eaf295612379ee20e72e1
lsankar4033 <lsankar4033@gmail.com>
2022-07-03 20:14:24 -0400

    Restructure daohack data

--- sql/dune/dao_hack_addresses.sql
+++
@@ -1,2 +0,0 @@
-SELECT DISTINCT "to" from thedao."DAO_evt_CreatedToken"
-            WHERE "evt_block_number" < 1718497;
---
+++ sql/dune/dao_hack_xfers.sql
@@ -0,0 +1,2 @@
+SELECT * from thedao."DAO_evt_Transfer" 
+            WHERE "evt_block_number" < 1718497;
